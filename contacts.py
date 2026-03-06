b0b0900c8142ba985aded96a94f89bd6d701769e
Mugilan10 <mugilanrajan02@gmail.com>
2026-03-06 12:20:13 +0530

    add delete contact feature

--- contacts.py
+++ contacts.py
@@ -14,6 +14,16 @@ def save_contacts():
     with open("contacts.json","w")as f:
         json.dump(contacts,f)
 
+def delete_contact(delete_name):
+    global contacts
+    original_length = len(contacts)
+    contacts = [c for c in contacts if delete_name.lower() not in c["name"].lower()]
+    if len(contacts) < original_length:
+        save_contacts()
+        print(f"Contact '{delete_name}' deleted.")
+    else:
+        print(f"No contact found with name '{delete_name}'.")
+
 def load_contacts():
     try:
         with open("contacts.json","r") as f:
@@ -32,19 +42,27 @@ contacts = load_contacts()
 
 while True:
     print("\n1. Add contact")
-    print("2. View contacts")
-    print("3. Search contact by name")
-    print("4. Quit")
+    print("2. Delete contact")
+    print("3. View all contacts")
+    print("4. Search contact by name")
+    print("5. Quit")
     choice = input("Enter choice: ")
+
     if (choice == "1"):
         name = input("Enter name: ")
         phone = input("Enter phone number: ")
         email = input("Enter your email: ")
         add_contact(name, phone, email)
         save_contacts()
-    elif (choice == "2"):
-        print(contacts)
+
+    elif(choice == "2"):
+        delete_name = input("Enter name of contact to delete: ")
+        delete_contact(delete_name)
+        
     elif (choice == "3"):
+        print(contacts)
+    
+    elif (choice == "4"):
         search_name = input("Enter name to search: ")
         results = search_contact(search_name)
         if len(results) == 0:
@@ -52,5 +70,6 @@ while True:
         else:
             for r in results:
                 print(f"{r['name']} - {r['phone']} - {r['email']}")
-    elif (choice == "4"):
+    
+    elif (choice == "5"):
         break
